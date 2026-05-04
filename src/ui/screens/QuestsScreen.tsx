18dfc32370d9fe9367ff04391de8621b99793238
nathanprocode <nathan.procode@gmail.com>
2026-05-05 01:40:44 +0200

    chore: remove binary map assets from repo and document manual local setup

---
+++ src/ui/screens/QuestsScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Screen } from "../components/Screen";
+
+export function QuestsScreen() {
+  return (
+    <Screen>
+      <Text style={{ color: "white" }}>Quêtes (placeholder Jour 2)</Text>
+    </Screen>
+  );
+}
